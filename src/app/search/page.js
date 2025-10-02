937857736ae4b748401b9ebcfcc84597b4d4973f
NKukame <NKukame@jhb.dvt.co.za>
2025-10-02 13:38:33 +0200

    Search Page Set Up

---
+++ src/app/search/page.js
@@ -0,0 +1,26 @@
+import '../App.css';
+import './Search.css';
+import Header from '../components/HeaderComp/Header';
+
+function SearchPage(){
+    return(
+        <>
+
+        <div className='search-container'>
+            <Header />
+
+            <div className='search-body'>
+
+                <h2>Search Page</h2>
+
+                
+
+            </div>
+
+        </div>
+        
+        </>
+    );
+}
+
+export default SearchPage;
